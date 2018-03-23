efa9956c6410cec774c5b225496b62b6767ef0f4
DavidLapes <32767783+davidlapes@users.noreply.github.com>
2018-03-23 16:09:42 +0100

    DBBuilder.SQL - Removed 1:N relationship Game <- Question. - Added N:N relationship Game <-> Question.

--- src/com/djenterprise/app/builder/dbbuilder.sql
+++ src/com/djenterprise/app/builder/dbbuilder.sql
@@ -3,10 +3,10 @@ CREATE DATABASE swinzdb;
 USE swinzdb;
 
 CREATE TABLE `User` (
-  `username` varchar(32) NOT NULL UNIQUE,
-  `password` varchar(32) NOT NULL,
-  `avatar` blob,
-  `alias` varchar(32) NOT NULL UNIQUE,
+  `username` VARCHAR(32) NOT NULL UNIQUE,
+  `password` VARCHAR(32) NOT NULL,
+  `avatar` BLOB,
+  `alias` VARCHAR(32) NOT NULL UNIQUE,
   PRIMARY KEY (`username`)
 );
 
@@ -20,15 +20,21 @@ CREATE TABLE `Answer` (
 CREATE TABLE `Question` (
   `questionid` INT NOT NULL AUTO_INCREMENT,
   `text` TEXT(256) NOT NULL,
-  `gameid` varchar(8) NOT NULL,
+  `gameid` VARCHAR(8) NOT NULL,
   PRIMARY KEY (`questionid`)
 );
 
 CREATE TABLE `Game` (
-  `gameid` varchar(8) NOT NULL,
-  `creator` varchar(32) NOT NULL,
+  `gameid` VARCHAR(8) NOT NULL,
+  `creator` VARCHAR(32) NOT NULL,
   `datecreated` DATETIME NOT NULL,
   PRIMARY KEY (`gameid`)
+);
+
+CREATE TABLE `GameQuestions` (
+  `gameid_fk` VARCHAR(8) NOT NULL,
+  `questionid_fk` INT NOT NULL,
+  PRIMARY KEY (`gameid_fk`, `questionid_fk`)
 );
 
 ALTER TABLE `Answer` ADD CONSTRAINT `Answer_fk0` FOREIGN KEY (`questionid`) REFERENCES `Question`(`questionid`);
@@ -36,6 +42,10 @@ ALTER TABLE `Answer` ADD CONSTRAINT `Answer_fk0` FOREIGN KEY (`questionid`) REFE
 ALTER TABLE `Question` ADD CONSTRAINT `Question_fk0` FOREIGN KEY (`gameid`) REFERENCES `Game`(`gameid`);
 
 ALTER TABLE `Game` ADD CONSTRAINT `Game_fk0` FOREIGN KEY (`creator`) REFERENCES `User`(`username`);
+
+ALTER TABLE `GameQuestions` ADD CONSTRAINT `Question_fk1` FOREIGN KEY (`questionid_fk`) REFERENCES `Question`(`questionid`);
+
+ALTER TABLE `GameQuestions` ADD CONSTRAINT `Game_fk1` FOREIGN KEY (`gameid_fk`) REFERENCES `Game`(`gameid`);
 
 INSERT INTO User (username, password, alias) VALUES (
   'David', '1234', 'David'
