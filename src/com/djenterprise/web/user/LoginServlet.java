15ca1ab66a4fb40ab3d211d9cf7c59931ad65aab
Zynnarion <32767752+Zynnarion@users.noreply.github.com>
2018-04-22 13:44:22 +0200

    Registration servlet 0.1

--- src/com/djenterprise/web/user/LoginServlet.java
+++ src/com/djenterprise/web/user/LoginServlet.java
@@ -1,11 +1,15 @@
 package com.djenterprise.web.user;
 
+import com.djenterprise.app.user.UserBO;
+import com.djenterprise.db.user.Login;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -22,7 +26,18 @@ public class LoginServlet extends HttpServlet {
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession();
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (username.isEmpty()|| password.isEmpty()){
 
+            }
+            UserBO user = new UserBO();
+            user.setUsername(username);
+            user.setPassword(password);
+            if(Login.testLogin(user)){
+                
+            }
         }
     }
 }
