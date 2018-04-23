96fb420a7575e42eb16c2267226a275084674123
DavidLapes <dave.lapes@gmail.com>
2018-04-23 18:00:44 +0200

    Unit tests Authentication

--- src/com/djenterprise/app/authentication/MatchingPatterns.java
+++ src/com/djenterprise/app/authentication/MatchingPatterns.java
@@ -2,7 +2,7 @@ package com.djenterprise.app.authentication;
 
 public class MatchingPatterns {
 
-    static final public String USERNAME_REGEX = "^[a-zA-Z0-9]$";
-    static final public String ALIAS_REGEX = "\\A[\\pL|[0-9]]{1,32}\\z";
-    static final public String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,32}$";
+    static final public String USERNAME_REGEX = "^[a-zA-Z0-9]{3,32}$";
+    static final public String ALIAS_REGEX = "\\A[\\pL|[0-9._-]]{3,32}\\z";
+    static final public String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,32}$";
 }
--- test/com/djenterprise/app/encryption/TestAuthentication.java
+++ test/com/djenterprise/app/encryption/TestAuthentication.java
@@ -1,6 +1,57 @@
 package com.djenterprise.app.encryption;
 
+import com.djenterprise.app.authentication.Registration;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestAuthentication {
 
-    //TODO tests
+    static final private String CORRECT_ALIAS = "951David_Frýdecký753";
+    static final private String WRONG_ALIAS = "David Frýdecký";
+    static final private String CORRECT_USERNAME = "456mrzynn159";
+    static final private String WRONG_USERNAME = "752mr zynn852";
+    static final private String CORRECT_PASSWORD = "MisterZynnarion123";
+    static final private String WRONG_PASSWORD = "Mister Zynnarion 123";
+
+    @Test
+    public void test01correctUsername() {
+        Assert.assertTrue(
+                Registration.checkUsername(CORRECT_USERNAME)
+        );
+    }
+
+    @Test
+    public void test02wrongUsername() {
+        Assert.assertTrue(
+                ! Registration.checkUsername(WRONG_USERNAME)
+        );
+    }
+
+    @Test
+    public void test03correctAlias() {
+        Assert.assertTrue(
+                Registration.checkAlias(CORRECT_ALIAS)
+        );
+    }
+
+    @Test
+    public void test04wrongAlias() {
+        Assert.assertTrue(
+                ! Registration.checkAlias(WRONG_ALIAS)
+        );
+    }
+
+    @Test
+    public void test05correctPassword() {
+        Assert.assertTrue(
+                Registration.checkPassword(CORRECT_PASSWORD)
+        );
+    }
+
+    @Test
+    public void test06wrongPassword() {
+        Assert.assertTrue(
+                ! Registration.checkPassword(WRONG_PASSWORD)
+        );
+    }
 }
