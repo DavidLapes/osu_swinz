23d5390e3877fe0618efeb17d5e95c31dc42bc51
DavidLapes <dave.lapes@gmail.com>
2018-05-02 21:02:43 +0200

    GameDAO jUnit

--- test/com/djenterprise/db/game/TestGameDAO.java
+++ test/com/djenterprise/db/game/TestGameDAO.java
@@ -9,17 +9,27 @@ import java.util.List;
 
 public class TestGameDAO {
 
+    static final private String PLAYER_ONE_ALIAS = "David";
+    static final private String PLAYER_TWO_ALIAS = "Chymja";
+    static final private String CREATOR_USERNAME = "David";
+    static final private String GAME_ID = "01234567";
+
     @Test
     public void test01CreateGame() {
         GameBO game = new GameBO();
-        game.setPlayerOne("David");
-        game.setPlayerTwo("Chymja");
-        game.setCreator("David");
-        game.setGameId(game.generateId());
+        game.setPlayerOne(PLAYER_ONE_ALIAS);
+        game.setPlayerTwo(PLAYER_TWO_ALIAS);
+        game.setCreator(CREATOR_USERNAME);
+        game.setGameId(GAME_ID);
         List<QuestionBO> questionBOList = QuestionDAO.getAllQuestions();
         List<QuestionBO> questions = new ArrayList<>();
         questions.add(questionBOList.get(0));
         questions.add(questionBOList.get(1));
         GameDAO.createGame(game, questions);
+    }
+
+    @Test
+    public void test02DeleteGame() {
+        GameDAO.deleteGame(GAME_ID);
     }
 }
